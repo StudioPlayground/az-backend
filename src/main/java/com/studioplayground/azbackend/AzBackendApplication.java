116bb0c938776df481159c0102ccb84cdf52d5bb
Jo <vfjdg12@gmail.com>
2023-03-18 13:58:00 +0900

    :tada: Start az

---
+++ src/main/java/com/studioplayground/azbackend/AzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studioplayground.azbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studioplayground/azbackend/AzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studioplayground.azbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
