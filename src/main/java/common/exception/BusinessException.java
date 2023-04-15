86a7dc176289a494b1de17a3e7380658e20d460b
Jo <vfjdg12@gmail.com>
2023-04-15 15:51:23 +0900

    :sparkles: Exception 추가

---
+++ src/main/java/common/exception/BusinessException.java
@@ -0,0 +1,14 @@
+package common.exception;
+
+/**
+ * 비즈니스 로직상에서 발생한 Exception
+ */
+public final class BusinessException extends AzitException {
+    public BusinessException(Throwable e) {
+        super(e);
+    }
+
+    public BusinessException(Throwable e, String message) {
+        super(e, message);
+    }
+}
---
+++ src/main/java/common/exception/ValidationException.java
@@ -0,0 +1,14 @@
+package common.exception;
+
+/**
+ * validate 에서 발생한 Exception
+ */
+public final class ValidationException extends AzitException {
+    public ValidationException(Throwable e) {
+        super(e);
+    }
+
+    public ValidationException(Throwable e, String message) {
+        super(e, message);
+    }
+}
