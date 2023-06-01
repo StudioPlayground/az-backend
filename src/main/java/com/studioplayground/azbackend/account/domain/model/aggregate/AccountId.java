38e1d52c74406468a9e20222c01d96a16a8512b4
pureliture <tkdgur1756@naver.com>
2023-06-01 16:11:56 +0900

    :busts_in_silhouette: AccountId of contructor 추가

--- src/main/java/com/studioplayground/azbackend/account/domain/model/aggregate/AccountId.java
+++ src/main/java/com/studioplayground/azbackend/account/domain/model/aggregate/AccountId.java
@@ -4,6 +4,9 @@ public record AccountId(
         Long id
 ) {
     public static AccountId EMPTY = new AccountId(null);
+    public static AccountId of(Long id) {
+        return new AccountId(id);
+    }
     public boolean isNull() {
       return id == null;
   }
